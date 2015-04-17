873f63920bdc84cac8f4c8b89fa064e841091d9a
Aurélien Rivet <aurelienrivet@hotmail.fr>
2015-04-17 21:59:47 +0000

    Adding bootstrap. Adding global application partial view directory.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '~> 3.3.4'
 
-gem 'compass-rails'
+#gem 'compass-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 	end
 
 	def new
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
 			render :action => 'new'
 		end
 	end
-
 
 	def edit
 		@user = User.find(params[:id])
